64d6f8dcc9dfb9e8dd32be60a67639ebbebe7e44
Péter Herpai <pherpai@chemaxon.com>
2024-12-01 20:34:30 +0100

    Add sumNumbers to sum numbers of array

--- common/utils.ts
+++ common/utils.ts
@@ -4,3 +4,7 @@ export function readLines(path: string): string[] {
     const lines = fs.readFileSync(path, 'utf-8');
     return lines.split("\n");
 }
+
+export function sumNumbers(accumulator: number, value: number): number {
+    return accumulator + value;
+}
--- src/day01/day01.ts
+++ src/day01/day01.ts
@@ -1,10 +1,12 @@
+import {sumNumbers} from "../../common/utils";
+
 export function solvePart1(lines: string[]): number {
     let {array1, array2} = getNumberArrays(lines);
     array1.sort();
     array2.sort();
 
     return array1.map((_, index) => Math.abs(array1[index] - array2[index]))
-        .reduce((sum, value) => sum + value, 0);
+        .reduce(sumNumbers, 0);
 }
 
 export function solvePart2(lines: string[]): number {
@@ -17,7 +19,7 @@ export function solvePart2(lines: string[]): number {
     return array1.map((num) => {
         const multi = numCounts.get(num)
         return num * (multi === undefined ? 0 : multi);
-    }).reduce((sum, value) => sum + value, 0);
+    }).reduce(sumNumbers, 0);
 }
 
 function getNumberArrays(lines: string[]) {
